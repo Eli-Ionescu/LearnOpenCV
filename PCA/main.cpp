d55f039625e6dff161fde91bad5b737201241da7
Eli-Ionescu <ionescu.e.f@gamil.com>
2020-04-21 22:26:51 +0200

    embedding projected

--- PCA/main.cpp
+++ PCA/main.cpp
@@ -4,6 +4,7 @@
 
 #include <vector>
 #include <iostream>
+#include <fstream>
 #include <dirent.h>
 #include <random>
 #include <typeinfo> // operator typeid
@@ -119,7 +120,6 @@ vector<Mat> get_patches(Mat image, vector<Point2i> points, int M)
 		int start_c = points[i].y - M / 2;
 		int end_c = points[i].y + M / 2;
 
-
 		if (start_c < 0 || start_r < 0 ||
 			end_r > image.rows || end_c > image.cols)
 		{
@@ -145,64 +145,18 @@ vector<Mat> get_patches(Mat image, vector<Point2i> points, int M)
 	return patches;
 }
 
-
 /*****************  Functions taken from the internet ************************/
-/* 				https://bytefish.de/blog/pca_in_opencv/                  	 */
 
-
-// Converts the images given in src into a row matrix.
-Mat asRowMatrix(const vector<Mat>& src, int rtype, double alpha = 1, double beta = 0) {
-    // Number of samples:
-    size_t n = src.size();
-    // Return empty matrix if no matrices given:
-    if(n == 0)
-        return Mat();
-    // dimensionality of (reshaped) samples
-    size_t d = src[0].total();
-    // Create resulting data matrix:
-    Mat data(n, d, rtype);
-    // Now copy data:
-    for(int i = 0; i < n; i++) {
-        //
-        if(src[i].empty()) {
-            string error_message = format("Image number %d was empty, please check your input data.", i);
-            // CV_Error(CV_StsBadArg, error_message);
-			cout << error_message;
-        }
-        // Make sure data can be reshaped, throw a meaningful exception if not!
-        if(src[i].total() != d) {
-            string error_message = format("Wrong number of elements in matrix #%d! Expected %d was %d.", i, d, src[i].total());
-            // CV_Error(CV_StsBadArg, error_message);
-			cout << error_message;
-        }
-        // Get a hold of the current row:
-        Mat xi = data.row(i);
-        // Make reshape happy by cloning for non-continuous matrices:
-        if(src[i].isContinuous()) {
-            src[i].reshape(1, 1).convertTo(xi, rtype, alpha, beta);
-        } else {
-            src[i].clone().reshape(1, 1).convertTo(xi, rtype, alpha, beta);
-        }
-    }
-    return data;
-}
-
-// Normalizes a given image into a value range between 0 and 255.
-Mat norm_0_255(const Mat& src) {
-    // Create and return normalized image:
-    Mat dst;
-    switch(src.channels()) {
-    case 1:
-        cv::normalize(src, dst, 0, 255, NORM_MINMAX, CV_8UC1);
-        break;
-    case 3:
-        cv::normalize(src, dst, 0, 255, NORM_MINMAX, CV_8UC3);
-        break;
-    default:
-        src.copyTo(dst);
-        break;
-    }
-    return dst;
+Mat formatImagesForPCA(const vector<Mat> &data)
+{
+	Mat dst(static_cast<int>(data.size()), data[0].rows * data[0].cols, CV_32F);
+	for (unsigned int i = 0; i < data.size(); i++)
+	{
+		Mat image_row = data[i].clone().reshape(1, 1);
+		Mat row_i = dst.row(i);
+		image_row.convertTo(row_i, CV_32F);
+	}
+	return dst;
 }
 
 /*********************************************************************************/
@@ -268,7 +222,7 @@ int main(int argc, char **argv)
 		class_patches.reserve(nr_samples);
 		class_patches = get_patches(src, samples[i], M);
 		cout << class_patches.size() << " patches extracted." << endl;
- 		patches.push_back(class_patches);
+		patches.push_back(class_patches);
 	}
 
 	cout << patches.size() << endl;
@@ -277,51 +231,51 @@ int main(int argc, char **argv)
 	// examples: patch re-sizing, PCA (e.g. from opencv), LDA, tSNE (nice tool, available online)
 	// For now, the data is stored like this:
 	// nr_clases(C) vectors, that contain nr_samples(N) Mat, each Mat of size MxM
-	// In order to use PCA, there is needed 
+	// In order to use PCA, there is needed
 	vector<Mat> all_patches;
 	all_patches.reserve(nr_classes * nr_samples);
 
 	vector<string> patch_labels;
 	patches.reserve(nr_classes * nr_samples);
-	for (int i = 0; i < patches.size(); i++) {
-		for (int j = 0; j < patches[i].size(); j++) {
+	for (int i = 0; i < patches.size(); i++)
+	{
+		for (int j = 0; j < patches[i].size(); j++)
+		{
 			all_patches.push_back(patches[i][j]);
 			patch_labels.push_back(label_names[i]);
 		}
 	}
 	cout << all_patches.size() << endl;
 
-	Mat data = asRowMatrix(all_patches, CV_32FC1);
+	Mat data = formatImagesForPCA(all_patches);
 
-	 // Number of components to keep for the PCA:
-    int num_components = 3;
+	// Number of components to keep for the PCA:
+	int num_components = 3;
 
-    // Perform a PCA:
-    PCA pca(data, Mat(), PCA::DATA_AS_ROW, num_components); // TODO(check how the data is handled row / col);
+	// Perform a PCA:
+	PCA pca(data, Mat(), PCA::DATA_AS_ROW, num_components); // TODO(check how the data is handled row / col);
 
-    // And copy the PCA results:
-    Mat mean = pca.mean.clone();
-    Mat eigenvalues = pca.eigenvalues.clone();
-    Mat eigenvectors = pca.eigenvectors.clone();
+	// And copy the PCA results:
+	Mat mean = pca.mean.clone();
+	Mat eigenvalues = pca.eigenvalues.clone();
+	Mat eigenvectors = pca.eigenvectors.clone();
 
 	cout << "Data: " << data.rows << " x " << data.cols << endl;
 	cout << "Mean: " << mean.rows << " x " << mean.cols << endl;
 	cout << "eigenvals " << eigenvalues.rows << " x " << eigenvalues.cols << endl;
 	cout << "eigenvectors " << eigenvectors.rows << " x " << eigenvectors.cols << endl;
 
-	for (int c=0; c< data.cols; c++) {
-		cout << data.at<Vec3f>(0, c) << endl;
+	std::ofstream vectors_file, metadata_file;
+	vectors_file.open("../vectors.tsv");
+	metadata_file.open("../metadata.tsv");
+	metadata_file << "Class\n";
+	// Print the vectors in a TSV for Embeddings projector http://projector.tensorflow.org/
+	for (int i = 0; i < data.rows; i++)
+	{
+		Mat point = pca.project(data.row(i)); // project into the eigenspace, thus the image becomes a "point"
+		vectors_file << point.at<float>(0) << "\t" << point.at<float>(1) << "\t" << point.at<float>(2) << "\n";
+		metadata_file << patch_labels[i] << "\n";
 	}
-
-    // // The mean face:
-    // imshow("avg", norm_0_255(mean.reshape(1, all_patches[0].rows)));
-
-    // // The first three eigenfaces:
-    // imshow("pc1", norm_0_255(pca.eigenvectors.row(0)).reshape(1, all_patches[0].rows));
-    // imshow("pc2", norm_0_255(pca.eigenvectors.row(1)).reshape(1, all_patches[0].rows));
-    // imshow("pc3", norm_0_255(pca.eigenvectors.row(2)).reshape(1, all_patches[0].rows));
-
-    // // // Show the images:
-    // waitKey(0);
-
+	vectors_file.close();
+	metadata_file.close();
 }
